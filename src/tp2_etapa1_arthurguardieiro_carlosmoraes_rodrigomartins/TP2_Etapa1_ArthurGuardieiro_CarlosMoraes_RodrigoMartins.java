6a8228ca45939f6cfb45800976614d9cc35194f6
Carlos Moraes <carloshrmoraes@gmail.com>
2022-07-19 22:05:29 -0300

    inicialização

---
+++ src/tp2_etapa1_arthurguardieiro_carlosmoraes_rodrigomartins/TP2_Etapa1_ArthurGuardieiro_CarlosMoraes_RodrigoMartins.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_etapa1_arthurguardieiro_carlosmoraes_rodrigomartins;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author MASTER
+ */
+public class TP2_Etapa1_ArthurGuardieiro_CarlosMoraes_RodrigoMartins {
+
+    
+    public static void main(String[] args) {
+        JFrame janelaPrincipal = new Loja();
+
+    }
+    
+}
